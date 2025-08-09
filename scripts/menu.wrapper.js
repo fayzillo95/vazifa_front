5f5c7dd4bcc58d4b3bd3431a7c6a6df7a59b9907
fayzillo95 <ovovovlululutvata@gmail.com>
2025-08-10 04:01:48 +0500

    panel

--- scripts/menu.wrapper.js
+++ scripts/menu.wrapper.js
@@ -1,7 +1,8 @@
-const menu = document.getElementsByClassName("menu")[0]
 const menuBtn = document.getElementById("menu-btn")
+const navList = document.getElementById("head-nav-list")
 
 function menuBthUpdate() {
+    navList.classList.toggle("show")
     if (menuBtn.classList.contains("fa-bars")) {
         menuBtn.classList.remove("fa-bars")
         menuBtn.classList.add("fa-times")
@@ -10,40 +11,20 @@ function menuBthUpdate() {
         menuBtn.classList.remove("fa-times")
         menuBtn.classList.add("fa-bars")
     }
+    console.log(navList.classList)
 }
-
-function removerNavItem(event) {
-    const width = window.innerWidth
-
-    if (width <= 768) {
-        menuBtn.classList.remove("none-disp")
-        if (menu.classList.contains("show")) {
-            menu.classList.remove("show")
-            menuBthUpdate()
-        }
-    } else {
-        if (menu.classList.contains("show")) {
-            menu.classList.remove("show")
-            menuBthUpdate()
-        }
-        menuBtn.classList.add("none-disp")
-    }
-    breakpoints.forEach(({point,element} ,index) => {
-        if (width <= point) {
-            menu.appendChild(element)
-        } else {
-            index > 3 ? iconList.appendChild(element) : navList.appendChild(element)
-        }
-    })
-}
-
-
-window.addEventListener('resize', (event) => removerNavItem(event))
-removerNavItem()
 
 
 menuBtn.addEventListener("click", (event) => {
-    console.log(menu.classList)
-    menu.classList.toggle("show")
     menuBthUpdate()
+})
+
+window.addEventListener("resize", () => {
+    if (navList.classList.contains("show")) {
+        navList.classList.remove("show")
+    }
+    else if (menuBtn.classList.contains("fa-times")) {
+        menuBtn.classList.remove("fa-times")
+        menuBtn.classList.add("fa-bars")
+    }
 })
